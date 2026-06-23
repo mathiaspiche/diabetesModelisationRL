4d1ec5bf3fa3cb9f1cdc3038f40f9898d9713611
mathiaspiche <mathiaspiche@hotmail.com>
2026-06-23 13:40:01 -0400

    Guard : bolus = basal = 0 when  bg <70 and when no meal, bolus = 0.

--- envs/myenv.py
+++ envs/myenv.py
@@ -8,9 +8,19 @@ class CustomT1DSimEnv(T1DSimEnv):
         basal = float(action[0])
         bolus = float(action[1])
 
-        if cho > 0:
-            self.patient._announce_meal(cho)
-            self.patient._last_foodtaken = cho
+
+        meal = self.scenario.get_action(self.time).meal
+
+        if meal == 0.0:
+            bolus = 0.0
+
+        if float(self.patient.observation.Gsub) < 70.0:
+            basal = 0.0
+            bolus = 0.0
 
         pump_action = PatientAction(basal=basal, bolus=bolus)
-        return super().step(pump_action)
+
+        step_result = super().step(pump_action)
+        step_result.info['delivered_basal'] = basal
+        step_result.info['delivered_bolus'] = bolus
+        return step_result
